c1d6e041113b8b062ce8fea0656b4aa234429b64
Robby Tan <robby-tan@outlook.com>
2017-10-03 12:29:41 +0700

    Wombat movement using input keyboard
    Wombat movement using input keyboard

--- Wombat.java
+++ Wombat.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Wombat here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Robby Tan
+ * @version 1.0
  */
 public class Wombat extends Actor
 {
@@ -13,13 +13,28 @@ public class Wombat extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() {
+        detectInput();
     }    
-    
+
     public void move() {
         move(1);
     }
-    
+
     public void turnLeft() {
         turn(-90);
+    }
+
+    public void detectInput() {
+        if (Greenfoot.isKeyDown("right")) {
+            turn(10);
+        } else if (Greenfoot.isKeyDown("up")) {
+            move(1);
+        } else if (Greenfoot.isKeyDown("left")) {
+            turn(-10);
+        } else if (Greenfoot.isKeyDown("down")) {
+            move(-1);
+        } else if (Greenfoot.isKeyDown("space")) {
+            move(10);
+        }
     }
 }
