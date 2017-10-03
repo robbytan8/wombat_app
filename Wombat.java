495dc5fa566534cc84e5032a1617c2fc0c87b028
Robby Tan <robby-tan@outlook.com>
2017-10-03 12:19:13 +0700

    Update wombat to move random or around the screen
    Update wombat to move random or around the screen

--- Wombat.java
+++ Wombat.java
@@ -13,13 +13,31 @@ public class Wombat extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() {
+        //  moveRandomly();
+        moveAtScreenEdge();
     }    
-    
+
     public void move() {
         move(1);
     }
-    
+
     public void turnLeft() {
         turn(-90);
+    }
+
+    public void moveRandomly() {
+        int randomNumber = Greenfoot.getRandomNumber(30);
+        if (randomNumber == 3) {
+            turnLeft();
+        }
+        move();
+    }
+
+    public void moveAtScreenEdge() {
+        if (getX() == 20 || getX() == getWorld().getWidth() - 20 || getY() == 20 || getY() ==  getWorld().getHeight() - 20) {
+            move(-1);
+            turnLeft();
+        }
+        move();
     }
 }
